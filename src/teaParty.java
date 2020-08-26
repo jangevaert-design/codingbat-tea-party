d6303e66d2bf7233bb8b372095536105a5ea32b6
Jan Gevaert <jan.gevaert@gmail.com>
2020-08-26 06:16:58 -0600

    more concise added

--- src/teaParty.java
+++ src/teaParty.java
@@ -9,3 +9,13 @@ public class teaParty {
     }
   }
 }
+/* more concise
+  public int teaParty(int tea, int candy) {
+  if (tea < 5 || candy < 5) {
+  return 0;
+  if ((tea >= 2 * candy || (candy >= 2 * tea)) {
+  return 2;
+  } else {
+  return 1;
+  }
+ */
